4114b28d31eca073f5861a776f1293e755a7c202
CookJustin <cookjpaypal@gmail.com>
2021-08-29 16:50:49 -0400

    Conference Application Using Maven, Will be updated to use Spring.

---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+
+    //Simulates going to database and returning a list of speakers
+    @Override
+    public List<Speaker> findAll(){ //returns the list of speakers
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+
+        speakers.add(speaker);
+
+        return speakers;
+    }
+
+}
